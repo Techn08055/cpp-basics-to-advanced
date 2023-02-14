dd55989960c970da435cc2f251543747c7a4ee31
christy <christy.varghese@ignitarium.com>
2023-02-14 15:00:44 +0530

    passing functions

---
+++ pass_by_ref/main.cpp
@@ -0,0 +1,18 @@
+// This brings in the iostream library 
+#include <iostream>
+
+void say_age (int &age){
+    ++age;
+    std::cout << "you are "<< &age <<std::endl;
+}
+//Entry point in main function
+int main()
+{
+    int age =24;
+    std::cout << "you are "<< age <<std::endl;
+    say_age(age);
+    std::cout << "you are "<< &age <<std::endl;
+
+    return 0;
+    //progran end 
+}
